6f2984afbb94ab64426b681fec83e84f364c3e4e
POLinA1234AAA <112808324+POLinA1234AAA@users.noreply.github.com>
2024-01-25 16:55:31 +0000

    creation of  blocks and container

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 function createBlock(width, height, initialOrder) {
     return {
+        color: null,
         width: width,
         height: height,
         isPlaced: false,
-        coordinates: { top: null, left: null, right: null, bottom: null },
-        color: null,
+        coordinates: { top: null,  right: null, bottom: null, left: null},
         initialOrder: initialOrder,
+    };
+}
+function createContainer(containerSize) {
+    return {
+        width: containerSize.width,
+        height: containerSize.height,
+        internalCavities: [
+            { top: 0, left: 0, right: containerSize.width, bottom: containerSize.height },
+        ],
     };
 }
